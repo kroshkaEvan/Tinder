de05d2ff649cd68d284a4e2c9ad0356187ccf995
Эван Крошкин <>
2022-06-02 12:54:44 +0300

    Initial project setup

---
+++ Tinder/Tinder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Tinder
+//
+//  Created by Эван Крошкин on 2.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
